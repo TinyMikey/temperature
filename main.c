75a93214a3a3ae17ec89bc4014d749e7ffbb81d1
michael <mtownsendnyc@protonmail.com>
2025-03-03 08:03:21 -0600

    Modified the initial program to use a conversion function.

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+int conversion();
+
 int main() {
+	printf("\nFahrenheit to Celsius.\n");
+	conversion();
+}
+
+int conversion() {
 	int step, lower, upper, fahr;
 	float cels;
 	step = 20;
 	lower = 0;
 	upper = 300;
 	
-	printf("Fahrenheit to Celsius.\n");
 	for(fahr = lower; fahr <= upper; fahr = fahr + step) {
 		cels = (fahr-32.0)/(9.0/5.0);
 		printf("\n%6d\t%9.1f", fahr, cels);
